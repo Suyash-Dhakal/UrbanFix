c8ca344862024dee6859412dc1f2dd4956b47a25
Suyash Dhakal <suyash.dhakal@icloud.com>
2025-05-11 23:17:41 +0545

    initial commit: db connection completed

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './db/connectDB.js';
+import authRoutes from './routes/auth.route.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.use('/api/auth',authRoutes);
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log(process.env.MONGO_URI);
+    
+    console.log(`Server is running on port :${PORT}`);
+});
+
+
+
+
+
+
+
